de1cd75a5d57b3a7de34236c6ae74dec7119c753
freddd1 <itamar.fradkin@gmail.com>
2022-05-09 19:36:51 +0300

    added `create_dataset_dict`

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,14 @@
 # Will hold all the datasets creations and manipultions
 
+import re
+import os
 import pandas as pd
+import datasets
+from datasets import load_dataset
 from sklearn.model_selection import StratifiedKFold
 
 
-def create_folds(df: pd.DataFrame, label_name: str, num_folds: int, seed: int = 11):
+def create_folds(df: pd.DataFrame, label_name: str, num_folds: int, seed: int = 11) -> pd.DataFrame:
     """
     The function will recive df, create StratifiedKfold and return df with new columns
     `folds` that will hold the fold number
@@ -24,3 +28,55 @@ def create_folds(df: pd.DataFrame, label_name: str, num_folds: int, seed: int = 
         df_f.loc[val_idx, 'fold'] = fold
     
     return df_f
+
+
+def preprocess(txt: str) -> str:
+    """
+    All the preprocessing of the text
+    """
+
+    # remove urls from tweet
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    txt = re.sub(url_regex, '', txt)
+
+    return txt
+
+
+def create_datasetdict(df:pd.DataFrame, fold: int) -> datasets.dataset_dict.DatasetDict:
+    """
+    The function creates DatasetDict according to the fold.
+    It temporery save the files in `data` folder and delets it.
+    """
+    assert df.fold.max() >= fold, f'fold: {fold} is out of range. max fold is: {df.fold.max()}'
+
+    # Helper function that will help us remove files.
+    def remove_file(filename: str):
+        if os.path.isfile(filename):
+            os.remove(filename)
+        
+    
+    train_filename = 'data/train_temp.csv'
+    test_filename = 'data/test_temp.csv'
+
+
+    remove_file(train_filename)
+    remove_file(test_filename)
+
+    df.dropna(inplace=True)
+
+    # split to train and test
+    train, test = df[df.fold != fold], df[df.fold == fold]
+    train = train.dropna()
+    test = test.dropna()
+
+    # temporery save them to for easy loading
+    train[['tweet', 'label']].to_csv(train_filename, index=False)
+    test[['tweet', 'label']].to_csv(test_filename, index=False)
+
+    # loading
+    dataset = load_dataset('csv', data_files={'train': train_filename, 'test': test_filename})
+
+    remove_file(train_filename)
+    remove_file(test_filename)
+
+    return dataset
