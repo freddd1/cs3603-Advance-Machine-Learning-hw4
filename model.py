14d420f03307794629c8aaaada02acf2d6e5ba8d
freddd1 <itamar.fradkin@gmail.com>
2022-05-12 09:50:05 +0300

    added `tokenize function`

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 # Will hold all the model unitils and the model itself
+
+
+
+def tokenize_function(examples, tokenizer):
+    # TODO: Im not sure why, but altough i removing NaN, we are stil encountering some nan values
+    # This is just a workaround that needs to be fixed
+    if not examples['tweet']:
+        examples['tweet'] = 'this is fake tweet'
+    return tokenizer(examples["tweet"], padding="max_length", truncation=True)
